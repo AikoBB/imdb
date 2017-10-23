18da86a8a16090897c7228215764bb5390a230a4
Aigerim Beishenbekova <aykobb@gmail.com>
2017-10-23 16:27:27 +0600

    deleting old files

--- src/com/aigerimbb/imdb/dao/person/Actress.java
+++
@@ -1,9 +0,0 @@
-package com.aigerimbb.imdb.dao.person;
-
-//in people.txt file there will be actresses and for this we have created this class
-public class Actress extends Performer{
-	public Actress(String id, String name, String surname, String country) {
-		super(id, name, surname, country);//this line passes actress' data to superclass
-		
-	}
-}
